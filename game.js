72c01e07cd31041f2aa647b561f3c41a67e3bc84
AdarshSinghTomar <veracryptic@gmail.com>
2022-09-06 23:59:01 +0530

    Still updating

--- game.js
+++ game.js
@@ -16,11 +16,13 @@ let game= new Phaser.Game(config);
 // now we will create functions
 // preload is used for image and animation loading
 function preload() {
-
+    this.load.image("ground","Assets/topground.jpg");
 }
 // create is called once preload has completed, this includes the loading of any assets from the Loader.
 //If you don't have a preload method then create is the first method called in your State.
 function create() {
+    W=game.config.width;
+    H=game.config.height;
 
 }
 // The update method is left empty for your own use. It is called during the core game loop AFTER debug, physics, plugins and the Stage have had their preUpdate methods called.
