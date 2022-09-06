ef95e6ed12155184bc64adbfd1f8ee5a9164b0ac
AdarshSinghTomar <veracryptic@gmail.com>
2022-09-06 23:52:43 +0530

    Update game.js

--- game.js
+++ game.js
@@ -14,12 +14,17 @@ let config={
 };
 let game= new Phaser.Game(config);
 // now we will create functions
+// preload is used for image and animation loading
 function preload() {
 
 }
+// create is called once preload has completed, this includes the loading of any assets from the Loader.
+//If you don't have a preload method then create is the first method called in your State.
 function create() {
 
 }
+// The update method is left empty for your own use. It is called during the core game loop AFTER debug, physics, plugins and the Stage have had their preUpdate methods called.
+//If is called BEFORE Stage, Tweens, Sounds, Input, Physics, Particles and Plugins have had their postUpdate methods called.
 function update()
 {
 
